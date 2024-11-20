84105ab2bf388bc6fde853bc6046bccd8908031f
ararso1 <arasoalisho2@gmail.com>
2024-11-20 23:50:27 +0300

    updated urls

--- frontend/src/components/InstructorRegistration.jsx
+++ frontend/src/components/InstructorRegistration.jsx
@@ -94,7 +94,7 @@ const InstructorRegistration = () => {
     try {
       // Submit form data to the server
       //console.log(formDataObj, 'dataaaaaaaaaaa')
-      await axios.post('http://localhost:5000/api/instructors', formDataObj, {
+      await axios.post('https://damiina.onrender.com/api/instructors', formDataObj, {
         headers: { 'Content-Type': 'multipart/form-data' },
       });
       setSuccessMessage('Your Information Submitted Successful!');
